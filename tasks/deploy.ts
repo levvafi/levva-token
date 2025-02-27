ae3a4b3d7fff8405029c966ab5d7b2748e972c0c
Ivan Sanin <ivan.sanin@eqlab.online>
2025-02-27 14:43:23 +0300

    depoy levva staking smart contracts

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,17 +1,11 @@
 import { task } from 'hardhat/config';
-import { ContractTransactionReceipt, ethers, formatEther, toUtf8Bytes } from 'ethers';
+import { ContractTransactionReceipt, ethers, formatEther, Provider, toUtf8Bytes } from 'ethers';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import {
-  LevvaToken,
-  LevvaToken__factory,
-  Staking,
-  Staking__factory,
-  VestingWalletFactory,
-  VestingWalletFactory__factory,
-} from '../typechain-types';
+import { LevvaToken, LevvaToken__factory, Staking, Staking__factory } from '../typechain-types';
 import * as fs from 'fs';
 import path from 'path';
 import { CREATE_X_ABI, CREATE_X_ADDRESS } from './createX';
+import { getSigner, SignerArgs, taskWithSigner, verifyContract } from './utils';
 
 interface DeployArgs {
   signer: string;
@@ -75,120 +69,65 @@ task('deploy', 'Deploy token')
     console.log(`Done!`);
   });
 
-// Example:
-// npx hardhat --network holesky --config hardhat.config.ts deploy-vesting-factory --signer <private-key>
-task('deploy-vesting-factory', 'Deploy vesting factory')
-  .addParam<string>('signer', 'Private key of contracts creator')
-  .setAction(async (taskArgs: DeployArgs, hre: HardhatRuntimeEnvironment) => {
-    const provider = hre.ethers.provider;
+// Example: npx hardhat --network holesky --config hardhat.config.ts deploy-levva-staking --keystore <keystore-file>
+taskWithSigner('deploy-levva-staking', 'Deploy Levva Staking smart contract').setAction(
+  async (taskArgs: SignerArgs, hre: HardhatRuntimeEnvironment) => {
+    const provider: Provider = hre.ethers.provider;
+    const signer = await getSigner(taskArgs, provider);
 
     const startBlockNumber = await provider.getBlockNumber();
-
     const networkName = hre.network.name;
     const configDir = `../deploy/${hre.network.name}`;
 
     console.log(`Deploy on network "${networkName}"`);
     console.log(`Current block number is ${startBlockNumber}\n\n`);
 
-    let signer = new hre.ethers.Wallet(taskArgs.signer, provider);
-    const balanceBefore = await signer.provider!.getBalance(signer);
+    const balanceBefore = await provider.getBalance(signer);
     console.log(`Balance before: ${formatEther(balanceBefore)} Eth`);
 
-    const contractId = 'VestingFactory';
-    const factory = (await new VestingWalletFactory__factory(signer).deploy(signer)) as any as VestingWalletFactory;
-    const deploymentTx = factory.deploymentTransaction()!;
-    await factory.waitForDeployment();
-    const txReceipt = await deploymentTx.wait();
-    const txHash = deploymentTx.hash;
+    // Levva Staking arguments
+    const TOKEN: string = '0x6243558a24CC6116aBE751f27E6d7Ede50ABFC76'; //LEVVA token
+    const owner: string = '0xea42f017a9D962019E36ce4D7d376D0421855b66'; //owner
+    const vault: string = '0xea42f017a9D962019E36ce4D7d376D0421855b66'; //vault
 
-    const contractAddress = await factory.getAddress();
+    // const APY: number = 120; // 120%
+    // const LOCK: number = 2628000; // 1 month
+
+    // const APY: number = 60;
+    // const LOCK: number = 15768000; // 6 month
+
+    const APY: number = 50;
+    const LOCK: number = 31536000;
+
+    const contractId = `LevvaStaking-${APY}-${LOCK}`;
+
+    const staking = (await new Staking__factory(signer).deploy(TOKEN, owner, vault, APY, LOCK)) as any as Staking;
+    const stakingAddress = await staking.getAddress();
+    const stakingDeploymentTx = staking.deploymentTransaction()!;
+    await staking.waitForDeployment();
+    const stakingTxReceipt = await stakingDeploymentTx.wait();
+    const stakingTxHash = stakingDeploymentTx.hash;
+
+    await verifyContract(hre, stakingAddress, [TOKEN, owner, vault, APY, LOCK]);
 
     const deploymentData = {
       [contractId]: {
-        address: contractAddress,
-        txHash,
-        blockNumber: deploymentTx.blockNumber!,
+        address: await staking.getAddress(),
+        txHash: stakingTxHash,
+        blockNumber: stakingDeploymentTx.blockNumber!,
       },
     };
 
     await saveDeploymentData(contractId, deploymentData, configDir);
 
-    await verifyContract(hre, contractAddress, [signer.address]);
-
     const balanceAfter = await signer.provider!.getBalance(signer);
     console.log(`Balance after: ${formatEther(balanceAfter)} Eth`);
-    console.log(`Gas used for deploy: ${txReceipt?.gasUsed} gas`);
+    console.log(`Gas used for deploy: ${stakingTxReceipt?.gasUsed} gas`);
 
     console.log(`Spent for deploy: ${formatEther(balanceBefore - balanceAfter)} Eth`);
     console.log(`Done!`);
-  });
-
-// Example:
-// npx hardhat --network holesky --config hardhat.config.ts deploy-create3 --signer <private-key>
-task('deploy-create3', 'Deploy token')
-  .addParam<string>('signer', 'Private key of contracts creator')
-  .setAction(async (taskArgs: DeployArgs, hre: HardhatRuntimeEnvironment) => {
-    const provider = hre.ethers.provider;
-
-    const tokenOwnerAddress = '0xB19ef98e52f1aB06E7f9E4Df7A8deF39665FaacC';
-    //const tokenOwnerAddress = '0xAD70a0ab951780fF3397882fc5372db83dEb0606';
-
-    const startBlockNumber = await provider.getBlockNumber();
-    const networkName = hre.network.name;
-    const configDir = `../deploy/${hre.network.name}`;
-
-    console.log(`Deploy on network with CreateX factory"${networkName}"`);
-    console.log(`Current block number is ${startBlockNumber}\n\n`);
-
-    let signer = new hre.ethers.Wallet(taskArgs.signer, provider);
-
-    const balanceBefore = await signer.provider!.getBalance(signer);
-    console.log(`Balance before: ${formatEther(balanceBefore)} Eth`);
-
-    const contractId = 'LevvaToken';
-    const tokenByteCode = new LevvaToken__factory(signer).bytecode;
-
-    const createXContract = await hre.ethers.getContractAt(CREATE_X_ABI, CREATE_X_ADDRESS, signer);
-
-    const salt = '0x05950b4e68f103d5abef20364de219a247e59c2300e5266fd1d7f8fb0360eac7'; //ethers.keccak256(toUtf8Bytes('levva salt'));
-
-    const create3Proxy = await createXContract['computeCreate3Address(bytes32,address)'](
-      salt,
-      '0x05950b4e68f103d5aBEf20364dE219a247e59C23'
-    );
-    console.log(`create3Proxy address is ${create3Proxy}`);
-
-    const constructorArgs = hre.ethers.AbiCoder.defaultAbiCoder().encode(['address'], [tokenOwnerAddress]);
-    const initCode = tokenByteCode.concat(constructorArgs.replace('0x', ''));
-    const contractAddress = await createXContract['deployCreate3(bytes32,bytes)'].staticCall(salt, initCode);
-    console.log(contractAddress);
-    // const tx = await createXContract['deployCreate3(bytes32,bytes)'](salt, initCode);
-    // const txReceipt: ContractTransactionReceipt = await tx.wait();
-    // const txHash = txReceipt.hash;
-
-    // console.log(`Contract address is ${contractAddress}`);
-
-    // const deploymentData = {
-    //   [contractId]: {
-    //     address: contractAddress,
-    //     txHash,
-    //     blockNumber: startBlockNumber,
-    //   },
-    // };
-
-    // await saveDeploymentData(deploymentData, configDir);
-
-    // if (networkName !== 'hardhat') {
-    //   await verifyContract(hre, contractAddress, []);
-    // }
-
-    // const balanceAfter = await signer.provider!.getBalance(signer);
-    // console.log(`Balance after: ${formatEther(balanceAfter)} Eth`);
-    // console.log(`Gas used for deploy: ${txReceipt?.gasUsed} gas`);
-
-    // console.log(`Spent for deploy: ${formatEther(balanceBefore - balanceAfter)} Eth`);
-    // console.log(`Done!`);
-  });
+  }
+);
 
 // Example:
 // npx hardhat --network holesky --config hardhat.config.ts deploy-open-staking --signer <private-key>
@@ -216,7 +155,6 @@ task('deploy-open-staking', 'Deploy open staking contract')
     const owner: string = '0xea42f017a9D962019E36ce4D7d376D0421855b66'; //LVVA owner
     const vault: string = '0x4fBc79d384235e59574A2ebB6c721E4B939Ce188'; // prev open staking parameter
 
-
     // const TOKEN: string = '0xaC3a8f70C421849Da72136DC7C8883984C2653B6'; // LVVA token
     // const owner: string = '0xea42f017a9D962019E36ce4D7d376D0421855b66'; // LVVA owner
     // const vault: string = '0x8dD5C9A136E1AbD616620ab4EFdFBfeCfc28b180'; // Levva Treasury
@@ -235,7 +173,6 @@ task('deploy-open-staking', 'Deploy open staking contract')
     // const contractId = 'LevvaStaking-1year';
     // const APY: number = 50; // 50%
     // const LOCK: number = 31_536_000; // 365 * 24 * 60 * 60 - 1 year
-
 
     const contractId = 'OpenStaking';
     const staking = (await new Staking__factory(signer).deploy(TOKEN, owner, vault, APY, LOCK, {
@@ -282,20 +219,4 @@ async function saveDeploymentData(
   const resolvedPath = path.resolve(__dirname, filename);
   fs.writeFileSync(resolvedPath, data, { flag: 'wx' });
   console.log(`\nDeployment data saved: ${resolvedPath}`);
-}
-
-async function verifyContract(hre: HardhatRuntimeEnvironment, address: string, constructorArguments: any[]) {
-  const isDryRun = hre.config.networks.hardhat.forking !== undefined;
-  if (!isDryRun) {
-    console.log(`Verify contract ${address} with constructor arguments: ${constructorArguments}`);
-
-    try {
-      await hre.run('verify:verify', {
-        address,
-        constructorArguments,
-      });
-    } catch (e) {
-      console.log(`Verify contract ${address} failed: ${e}`);
-    }
-  }
 }
---
+++ tasks/utils.ts
@@ -0,0 +1,91 @@
+import { prompts } from 'prompts';
+import { task } from 'hardhat/config';
+import { ConfigurableTaskDefinition, HardhatRuntimeEnvironment } from 'hardhat/types';
+import { ethers, formatEther, parseEther, Signer } from 'ethers';
+import * as fs from 'fs';
+
+export interface SignerArgs {
+  privateKey?: string;
+  keystore?: string;
+  keystorePassword?: string;
+}
+
+export interface DeployArgs extends SignerArgs {
+  networkName: string;
+  impersonateSigner?: string;
+}
+
+export const taskWithSigner = (name: string, description?: string): ConfigurableTaskDefinition =>
+  task(name, description)
+    .addOptionalParam<string>('privateKey', 'Private key of contracts creator')
+    .addOptionalParam<string>('keystore', 'Keystore file path')
+    .addOptionalParam<string>('keystorePassword', 'Keystore file password');
+
+export const deployTask = (name: string, description?: string): ConfigurableTaskDefinition =>
+  taskWithSigner(name, description)
+    .addParam<string>('networkName', 'Network name')
+    .addOptionalParam<string>('impersonateSigner', 'Impersonate address for dry-run');
+
+const readSensitiveData = async (label: string): Promise<string> => {
+  const response = await prompts.invisible({
+    type: 'invisible',
+    name: 'result',
+    message: label,
+  });
+  return response as string;
+};
+
+export async function getSigner(taskArgs: SignerArgs, provider?: ethers.Provider | null): Promise<ethers.Wallet> {
+  let signer: ethers.Wallet;
+
+  if (taskArgs.privateKey) {
+    console.warn('\n!!! Using private key in plain text is not recommended\n');
+
+    signer = new ethers.Wallet(taskArgs.privateKey);
+  } else if (taskArgs.keystore) {
+    let keystorePassword = '';
+
+    if (taskArgs.keystorePassword) {
+      console.warn('\n!!! Use interactive mode to enter keystore password\n');
+
+      keystorePassword = taskArgs.keystorePassword;
+    } else {
+      keystorePassword = await readSensitiveData('Enter keystore password');
+    }
+    const jsonKeystore = fs.readFileSync(taskArgs.keystore, 'utf8');
+
+    const wallet = ethers.Wallet.fromEncryptedJsonSync(jsonKeystore, keystorePassword) as ethers.Wallet;
+    if (!wallet) {
+      throw new Error('Could not create wallet from keystore');
+    }
+
+    signer = provider ? wallet.connect(provider) : wallet;
+  } else {
+    const privateKey = await readSensitiveData('Enter signer private key');
+    signer = new ethers.Wallet(privateKey, provider);
+  }
+
+  return signer;
+}
+
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function verifyContract(hre: HardhatRuntimeEnvironment, address: string, constructorArguments: any[]) {
+  const isDryRun = hre.config.networks.hardhat.forking !== undefined;
+  if (isDryRun) return;
+
+  await delay(5000);
+
+  console.log(`Verify contract ${address} with constructor arguments: ${constructorArguments}`);
+
+  try {
+    await hre.run('verify:verify', {
+      address,
+      constructorArguments,
+    });
+  } catch (e) {
+    console.log(`Verify contract ${address} failed: ${e}`);
+  }
+}
